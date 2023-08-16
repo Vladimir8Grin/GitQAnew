4b1c3f7edfd27c8ff9b0a515185f0e689cc7257a
“Vladimir_Grin” <mortywallet@gmail.com>
2023-08-16 23:06:25 +0300

    gitQA2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13_458;
+        int bonus_one_miles = 44;
+        int finish_bonus;
+        finish_bonus = ticket / bonus_one_miles;
+        System.out.println("Количество бонусных миль равно - " + finish_bonus);
+    }
+}
